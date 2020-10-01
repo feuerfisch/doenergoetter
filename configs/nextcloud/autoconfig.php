43b0d124387cf7badbaae6e2258cc25cefb314b8
Martin Fischer <feuerfisch@users.noreply.github.com>
2020-10-01 13:34:18 +0200

    Add files via upload

---
+++ configs/nextcloud/autoconfig.php
@@ -0,0 +1,9 @@
+<?php
+$AUTOCONFIG = [
+  "dbtype"	=> "sqlite",
+  "dbname"	=> "nextcloud",
+  "dbtableprefix" => "",
+  "adminlogin" => "admin",
+  "adminpass" => "admin",
+  "directory" => "/var/www/html/data",
+];
